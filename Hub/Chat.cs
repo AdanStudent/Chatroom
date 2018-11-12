5a2bd7159bebd671ff0e2a276d6c216fc8a6e156
adan.benitez <adan.benitez@loop.colum.edu>
2018-11-12 14:27:05 -0600

    Add project files.

---
+++ Hub/Chat.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class Chat : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, string.Format("{0}, signed {1}", message, "please give us an A on my project please"));
+
+            //Call 
+            ParseMessage(message);
+        }
+
+        //public void BroadcastMessage(string message)
+        //{
+        //    Clients.All.SendAsync("broadcastMessage",  string.Format("{0}, signed {1}", message, "please give us an A on my project please"));
+
+
+        //}
+
+        public void ParseMessage(string message)
+        {
+
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Tree_Pt2;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Tree_Pt2.Program.Main2(args);
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
